58e2064453b22acd12d7a04fc6be4b0433760527
FvgbM <153570081+FvgbM@users.noreply.github.com>
2024-04-14 13:52:21 -0300

    Add files via upload
    feat(p1-pdmn): cria componente de entrada

---
+++ LembreteEntrada.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function LembreteEntrada() {
+
+    const [lembrete, setLembrete] = useState('');
+    const [lembretes, setLembretes] = useState([]);
+
+
+    const handleInputChange = (event) => {
+        setLembrete(event.target.value);
+    };
+
+    const adicionarLembrete = () => {
+        if (lembrete !== '') 
+        {
+            setLembretes([...lembretes, lembrete]);
+            setLembrete('');
+        }
+    };
+
+    return (
+        <div>
+            <h2>Adicionar Lembrete</h2>
+            <input
+                type="text"
+                value={lembrete}
+                onChange={handleInputChange}
+                placeholder="Digite um lembrete..."
+            />
+            <button onClick={adicionarLembrete}>Adicionar</button>
+            
+            <h3>Lembretes Cadastrados:</h3>
+            <ul>
+                {lembretes.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default LembreteEntrada;
+
+
