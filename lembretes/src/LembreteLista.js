722f5d6a84a2fb276ca447bedd95411dbe3a639f
joao marchette <jvmarchette02003@gmail.com>
2024-04-15 19:33:53 -0300

    Remoção dos lembretes fictícios Implementação do botão de inserção Implementação do campo de texto Adição de novos comentários Estilização feita em alguns pontos do projeto

---
+++ lembretes/src/LembreteLista.js
@@ -0,0 +1,37 @@
+// Importar a biblioteca React e o hook useState do React
+import React, { useState } from 'react';
+
+// Importar o componente LembreteEntrada do arquivo './LembreteEntrada'
+import LembreteEntrada from './LembreteEntrada';
+
+// Definição do componente funcional TarefaLista
+const TarefaLista = () => {
+  // Inicializar um array vazio 'tarefas' (tarefas) usando o hook useState
+  const [tarefas, setTarefas] = useState([]);
+
+  // Função para lidar com a adição de um novo lembrete
+  const handleAddReminder = (newReminder) => {
+    // Atualizar o array 'tarefas' adicionando um novo objeto com id e descricao (descrição)
+    setTarefas([...tarefas, { id: tarefas.length + 1, descricao: newReminder }]);
+  };
+
+  // Declaração JSX return para renderizar o componente
+  return (
+    <div className="container mt-4" style={{ borderRadius: '80px' }}>
+      <table className="table table-bordered" style={{ borderRadius: '80px' }}>
+        <tbody>
+          {/* Renderizar lembretes existentes e novos */}
+          {tarefas.map((tarefa) => (
+            <tr key={tarefa.id}>
+              <td className="py-3 rounded-pill border-2" style={{ backgroundColor: 'white', borderRadius: '15px' }}>{tarefa.descricao}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <LembreteEntrada onAddReminder={handleAddReminder} />
+    </div>
+  );
+};
+
+// Exportar o componente TarefaLista como exportação padrão
+export default TarefaLista;
---
+++ lembretes/src/index.js
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import LembreteEntrada from './LembreteEntrada'
+import TarefaLista from './LembreteLista'
+import "./personalizacao.css";
+
+const App = () => {
+    return (
+        // container principal
+        <div className="container border rounded mt-2" style={{ backgroundColor: 'palegreen', borderRadius: "10px" }}>
+            {/* linha para o título */}
+            <div className="row border-bottom m-2">
+                <h1 className="display-5 text-center"><TarefaLista /></h1>
+            </div>
+            {/* sub-linha*/}
+            <div className="row justify-content-center">
+                <div className="col-sm d-flex justify-content-center">
+                    Conteúdo da primeira coluna aqui
+                </div>
+                <div className="col-sm d-flex justify-content-center">
+                    Conteúdo da segunda coluna aqui
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+const App2 = () => {
+    return (
+        <div>
+            <LembreteEntrada />
+        </div>
+    );
+
+}
+export default App2;
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
