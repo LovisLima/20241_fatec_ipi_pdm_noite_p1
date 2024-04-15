4d274d22f23028d45af7e397a5e98b0d61b3325d
Renan Meira <renan.meira@gmail.com>
2024-04-14 22:19:27 -0300

    feat(p1-pdmn): exibe lista ficticia

---
+++ lembretes/src/LembreteLista.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+//Declara a lista de tarefas fictícias
+const TarefaLista = () => {
+    const tarefas = [
+        { id: 1, descricao: 'Preparar aula de programação' },
+        { id: 2, descricao: 'Fazer feira' },
+        { id: 3, descricao: 'Preparar marmitas' }
+    ];
+    //Retorna a lista de tarefas
+    //Função map para percorrer a lista de tarefas
+    return (
+        //container mt-4 centraliza e define a margem superior
+        //table table-bordered tabela com bordas
+        //py-3 preenchimento vertical nas células
+        <div className="container mt-4">
+            <table className="table table-bordered">
+                <tbody>
+                    {tarefas.map(tarefa => (
+                        //O atributo key é usado para identificar exclusivamente cada elemento na lista e é importante para a eficiência do React na renderização de listas.
+                        <tr key={tarefa.id}>
+                            <td className="py-3">{tarefa.descricao}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TarefaLista;
--- lembretes/src/index.js
+++ lembretes/src/index.js
@@ -1,30 +1,42 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import LembreteEntrada from './LembreteEntrada'
+import TarefaLista from './LembreteLista'
 
 const App = () => {
     return (
         // container principal
-        <div className="container border rounded mt-2" style={{backgroundColor: 'palegreen'}}>
-        {/* linha para o título */}
+        <div className="container border rounded mt-2" style={{ backgroundColor: 'palegreen' }}>
+            {/* linha para o título */}
             <div className="row border-bottom m-2">
-                <h1 className="display-5 text-center">Hello, lembretes</h1>
+                <h1 className="display-5 text-center"><TarefaLista /></h1>
             </div>
-        {/* sub-linha*/}
-        <div className="row justify-content-center">
-            <div className="col-sm d-flex justify-content-center">
-                Conteúdo da primeira coluna aqui
-            </div>
-            <div className="col-sm d-flex justify-content-center">
-                Conteúdo da segunda coluna aqui
+            {/* sub-linha*/}
+            <div className="row justify-content-center">
+                <div className="col-sm d-flex justify-content-center">
+                    Conteúdo da primeira coluna aqui
+                </div>
+                <div className="col-sm d-flex justify-content-center">
+                    Conteúdo da segunda coluna aqui
+                </div>
             </div>
         </div>
+
+    );
+}
+
+const App2 = () => {
+    return (
+        <div>
+            <LembreteEntrada />
         </div>
     );
-    }
+
+}
+export default App2;
 
 ReactDOM.render(
-<App />,
-document.querySelector('#root')
+    <App />,
+    document.querySelector('#root')
 )
